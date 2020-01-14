c1a3e54d4a688a164e8845c63ca4f1b4fa575648
Jun jae hyeong <vnfmfms00@gmail.com>
2020-01-14 11:38:46 +0900

    1st Commit
    적군 1초마다 생성, 적군은 플레이어 추적

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject target;
+    [SerializeField] float speed = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Player");
+        transform.eulerAngles = new Vector3(0, 0, -getAngle(transform.position.x,
+            transform.position.y, target.transform.position.x, target.transform.position.y));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+    }
+
+    private float getAngle(float x1, float y1, float x2, float y2)
+    {
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+
+        float rad = Mathf.Atan2(dx, dy);
+        float degree = rad * Mathf.Rad2Deg;
+
+        return degree;
+    }
+}
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Respawn : MonoBehaviour
+{
+    float time = 0f;
+    int cnt = 0;
+    float gameTime;
+    public GameObject prefab;
+    Text score;
+    Text timeRecord;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameTime = 0.0f;   // 게임 시간 측정
+        score = GameObject.Find("Canvas").transform.Find("Enemy_cnt_text").GetComponent<Text>();  // 컴포넌트 연결
+        timeRecord = GameObject.Find("Canvas").transform.Find("Time_text").GetComponent<Text>(); // 연결
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        // 실시간 시간을 더합니다.
+        if (time > 1.0)  // 만약 1초가 지났다면,
+        {
+            time -= 1.0f;       // 1초를 빼고
+            respawnMissile();   // respawnMissile() 이라는 함수를 호출합니다.
+            cnt++;              // 미사일이 추가될 때 마다 카운트를 올립니다.
+        }
+
+        score.text = "Enemy Count : " + cnt.ToString();    // 리스폰시 사용하는 카운트를 연결
+        timeRecord.text = "Time : " + gameTime.ToString("N2");   // 제한 시간을 연결합니다. N2는 소숫점 둘째짜리까지 출력
+        gameTime += Time.deltaTime;     // 실시간으로 증가시킵니다.
+    }
+
+    void respawnMissile()
+    {
+        float x = 14f;
+        float y = 5.47f;
+
+        int rand = Random.Range(0, 4);
+        // 4방향 중 한 군데를 랜덤으로 호출합니다. 0부터 3까지 랜덤입니다.
+        // 예 : 0 > 왼쪽 1 > 오른쪽 2 > 위쪽 3 > 아래쪽
+        switch (rand)
+        {
+            case 0:
+                newUnit(-x, Random.Range(0.0f, y * 2) - y);
+                break;
+            case 1:
+                newUnit(x, Random.Range(0.0f, y * 2) - y);
+                break;
+            case 2:
+                newUnit(Random.Range(0.0f, x * 2) - x, y);
+                break;
+            case 3:
+                newUnit(Random.Range(0.0f, x * 2) - x, -y);
+                break;
+            default:
+                Debug.Log("I don't care. :D ");
+                break;
+        }
+    }
+
+    void newUnit(float x, float y)
+    {
+        Instantiate(prefab, new Vector3(x, y, 0), Quaternion.identity);
+    }
+}
